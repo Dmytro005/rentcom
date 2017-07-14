c6a18491082c2097e417f3d756a4aefea4d75007
Dmytro005 <harazdovskiy@gmail.com>
2017-07-14 13:52:10 +0300

    Add files via upload
    Fix cross bags
    Fix mobile pop-ups bugs
    Change menu size

---
+++ rencom/js/main.js
@@ -0,0 +1,106 @@
+//Popup fade in and fade out function
+function Show($this) {
+    //Popup write us Animation
+    $('#PopupWriteUs').removeClass('animated fadeOutUpBig');
+    $('#PopupWriteUs').addClass('animated fadeInDownBig');
+    //////////////////////////
+
+    $id = "Popup" + $this.id;
+    //    alert($id);
+
+    ////Sets top position
+//    if ($('body').width() < 960) {
+//        $("#" + $id).css({
+//            "top": $(window).scrollTop()
+//        });
+//    } else {
+//        $("#" + $id).css({
+//            "top": $(window).scrollTop() + 100
+//        });
+//    }
+
+    ////Shows Popup
+    $("#" + $id).css({
+        "display": "flex"
+    });
+    ////Show overlay
+    $(".overlay").fadeIn(500);
+    ////Block body scroll
+    $("body").css({
+        "overflow": 'hidden'
+    });
+};
+
+function Hide() {
+    //Popup write us Animation
+    $('#PopupWriteUs').addClass('animated fadeOutUpBig');
+    ///////Hide overlay
+    $(".overlay").fadeOut(500);
+    /////Hide popup
+    $(".popup").fadeOut(500);
+    ////Enable body scroll
+    $("body").css({
+        "overflow": 'auto'
+    });
+};
+
+
+$(".btn, .btn2").click(function () {
+    Show(this);
+});
+
+$(".overlay").click(function () {
+    Hide();
+});
+
+$(".hide").click(function () {
+    Hide();
+});
+
+///////////////////////////////////
+
+$(document).ready(function () {
+    ///Gradualy scroll
+
+    $(".nav").mPageScroll2id();
+
+    ///Menu Scroll Animation
+
+    if ($('body').width() > 960) {
+        var options = {
+          offset: 700
+        }
+        var header = new Headhesive('.header', options);
+
+    }else{};
+
+
+/////////////////
+
+//Form load
+
+$("#form").submit(function () {
+    $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+    }).done(function () {
+        alert("Дякую за заявку, ми з вами скоро зв'яжемось!");
+    });
+    return false;
+});
+
+/////////////////
+
+});
+////////////////////////////////////
+/// Mobile menu toggle
+$('#toggle').click(function () {
+    $(this).toggleClass('ToggleActive');
+    $("#menu").toggleClass('MenuActive');
+});
+/////////ContactFormButton
+$("#ContactFormBtn").click(function () {
+    //        Hide();
+});
+//////////////////////////
